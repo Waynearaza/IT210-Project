162b4dcda8995abe19a8d58acc71afa0425e7c74
Wayne Araza <aerionne.araza@olivarezcollegetagaytay.edu.ph>
2026-05-06 20:04:48 +0800

    Added Inner Pages for three sections

--- js/script.js
+++ js/script.js
@@ -1,41 +1,43 @@
-$(document).ready(function() {
-            
-    // 1. Custom Sticky Navbar Background Change on Scroll
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('.navbar-custom').addClass('scrolled');
-        } else {
-            $('.navbar-custom').removeClass('scrolled');
-        }
-    });
+$(document).ready(function () {
+  // 1. Custom Sticky Navbar Background Change on Scroll
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".navbar-custom").addClass("scrolled");
+    } else {
+      $(".navbar-custom").removeClass("scrolled");
+    }
+  });
 
-    // 2. Custom Smooth Scrolling for Navigation Links
-    $('a.nav-link').on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            // Animate scroll to the target section, offset by navbar height (approx 70px)
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top - 70 
-            }, 800);
-        }
-    });
+  // 2. Custom Smooth Scrolling for Navigation Links
+  $("a.nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      // Animate scroll to the target section, offset by navbar height (approx 70px)
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 70,
+        },
+        800,
+      );
+    }
+  });
 
-    // 3. Custom Dynamic Filter for "Where to Go" Section
-    $('.filter-btn').on('click', function() {
-        // Update active button styling
-        $('.filter-btn').removeClass('active');
-        $(this).addClass('active');
+  // 3. Custom Dynamic Filter for "Where to Go" Section
+  $(".filter-btn").on("click", function () {
+    // Update active button styling
+    $(".filter-btn").removeClass("active");
+    $(this).addClass("active");
 
-        // Get filter value
-        var filterValue = $(this).attr('data-filter');
+    // Get filter value
+    var filterValue = $(this).attr("data-filter");
 
-        // Filter logic
-        if(filterValue === 'all') {
-            $('.filter-item').show('slow');
-        } else {
-            $('.filter-item').hide();
-            $('.filter-item.' + filterValue).show('slow');
-        }
-    });
+    // Filter logic
+    if (filterValue === "all") {
+      $(".filter-item").show("slow");
+    } else {
+      $(".filter-item").hide();
+      $(".filter-item." + filterValue).show("slow");
+    }
+  });
 });
